ce5ee76b19c11510c9aebe2c7c1c69173a7b7ffc
Sharon Cichelli <sharon@invisible-city.com>
2012-10-28 18:25:42 -0500

    react to button interrupt

--- GirlGeniusClank/GirlGeniusClank.ino
+++ GirlGeniusClank/GirlGeniusClank.ino
@@ -3,22 +3,58 @@ Servo robot;
 long lastTime;
 int quietPeriod = 500;
 
+volatile int state = LOW;
+
 void setup()
 {
   robot.attach(9);
   robot.write(0);
   lastTime = 0;
+  attachInterrupt(0, react, RISING);
 }
 
 void loop()
 {
   robot.write(0);
+  if (state == HIGH)
+  {
+    emote();
+    state = LOW;
+    delay(2000);
+    resetQuietPeriod();
+  }
   if (millis() - lastTime > quietPeriod)
   {
     fidget();
-    quietPeriod = random(5, 25) * 100;
-    lastTime = millis();
+    resetQuietPeriod();
   }
+}
+
+void resetQuietPeriod()
+{
+  quietPeriod = random(9, 25) * 100;
+  lastTime = millis();
+}
+
+void react()
+{
+  state = HIGH;
+}
+
+void emote()
+{
+  robot.write(160);
+  delay(100);
+  robot.write(80);
+  delay(100);
+  robot.write(160);
+  delay(100);
+  robot.write(80);
+  delay(100);
+  robot.write(160);
+  delay(100);
+  robot.write(80);
+  delay(2000);
 }
 
 void fidget()
