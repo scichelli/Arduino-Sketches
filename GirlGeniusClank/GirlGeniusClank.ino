57bfb8850d8a55ad9c0be89052a28ecdd84403f2
Sharon Cichelli <sharon@invisible-city.com>
2012-11-01 22:36:11 -0500

    refactor to state machine, to remove delay()s

--- GirlGeniusClank/GirlGeniusClank.ino
+++ GirlGeniusClank/GirlGeniusClank.ino
@@ -1,67 +1,87 @@
 #include <Servo.h>
+#define QUIET 0
+#define TWITCHINGR 1
+#define TWITCHINGL 2
+#define FIDGETING 3
+#define EMOTING 4
 
+volatile int state = QUIET;
 Servo robot;
+unsigned long lastTime = 0;
+int interval;
 int i = 0;
-long lastTime;
-int quietPeriod = 0;
-const int quietFactor = 100;
+
 //durations
 const int quick = 100;
 const int medium = 500;
 const int slow = 1000;
 //positions
 int center = 90;
-int top = 90 + 5;
-int bottom = 90 - 5;
-volatile int state = LOW;
+int range = 5;
+int top = center + range;
+int bottom = center - range;
+
+int emotePositions[16] = {
+   top, quick, center, quick, 
+   top, quick, center, quick, 
+   bottom, quick, center, quick,
+   bottom, quick,
+   center, slow};
+int fidgetPositions[16] = {
+  bottom, quick, top, medium, 
+  center, quick, top, quick, 
+  center, quick, top, quick,
+  bottom, slow, center, slow};
+int twitchLpositions[4] = {bottom, quick, center, medium};
+int twitchRpositions[4] = {top, quick, center, medium};
 
 void setup()
 {
   robot.attach(9);
-  robot.write(center);
-  lastTime = millis();
   attachInterrupt(0, react, RISING);
+  
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  if (state == HIGH)
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastTime > interval) 
   {
-    emote();
-    state = LOW;
+    lastTime = currentMillis;
+    doState();
   }
-  if (millis() - lastTime > quietPeriod)
+}
+
+void doState()
+{
+  Serial.println(state);
+  
+  switch(state)
   {
-    i = random(1, 5);
-    if (i == 1) {
-      fidget();
-    } else if (i == 2 || i == 3) {
-      twitchLeft();
-    } else {
+    case QUIET:
+      randomBehavior();
+      break;
+    case TWITCHINGR:
       twitchRight();
-    }
+      break;
+    case TWITCHINGL:
+      twitchLeft();
+      break;
+    case FIDGETING:
+      fidget();
+      break;
+    case EMOTING:
+      emote();
+      break;
+    default:
+      goQuiet();
   }
-}
-
-void resetQuietPeriod()
-{
-  quietPeriod = random(5, 25) * quietFactor;
-  lastTime = millis();
-}
-
-void move(int positions[], int numberOfPositions)
-{
-  for (i = 0; i < numberOfPositions; i += 2)
-  {
-    robot.write(positions[i]);
-    delay(positions[i+1]);
-  }
-  resetQuietPeriod();
 }
 
 void react()
 {
-  state = HIGH;
+  state = EMOTING;
 }
 
 void bweep()
@@ -73,42 +93,72 @@ void bweep()
   }
 }
 
-void emote()
+void randomBehavior()
 {
-  bweep();
-  const int num = 16;
-  int positions[num] = {
-     top, quick, center, quick, 
-     top, quick, center, quick, 
-     bottom, quick, center, quick,
-     bottom, quick,
-     center, slow * 2};
-  move(positions, num);
-}
-
-void fidget()
-{
-  const int num = 16;
-  int positions[num] = {
-    bottom, quick, top, medium, 
-    center, quick, top, quick, 
-    center, quick, top, quick,
-    bottom, slow, center, slow};
-  move(positions, num);
+  state = random(1, 3);
 }
 
 void twitchLeft()
 {
-  twitch(bottom);
+  if (i > 3)
+  {
+    goQuiet();
+    return;
+  }
+  move(twitchLpositions);
 }
 
 void twitchRight()
 {
-  twitch(top);
+  if (i > 3)
+  {
+    goQuiet();
+    return;
+  }
+  move(twitchRpositions);
 }
 
-void twitch(int firstDirection)
+void fidget()
 {
-  int positions[4] = {firstDirection, quick, center, medium};
-  move(positions, 4);
+  if (i > 16)
+  {
+    goQuiet();
+    return;
+  }
+  move(fidgetPositions);
 }
+
+void emote()
+{
+  if (i > 16)
+  {
+    goQuiet();
+    return;
+  }
+  if (i == 0)
+  {
+    bweep();
+  }
+  move(emotePositions);
+}
+
+void goQuiet()
+{
+  i = 0;
+  state = QUIET;
+  interval = random(5, 25) * 100;
+}
+
+void move(int positions[])
+{
+  Serial.print("To: ");
+  Serial.println(positions[i]);
+  Serial.print("Interval: ");
+  Serial.println(positions[i+1]);
+
+  robot.write(positions[i]);
+  delay(10);
+  interval = positions[i+1];
+  i += 2;
+}
+
