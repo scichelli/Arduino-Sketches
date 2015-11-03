89de62be6524891630a16c4b46414841b825e7e1
Sharon Cichelli <girlwritescode@gmail.com>
2015-11-03 08:01:28 -0600

    play a rising tone from a simple speaker, on repeat

---
+++ SoundExamples/speaker/speaker.ino
@@ -0,0 +1,15 @@
+#define SPEAKER 8
+int i = 0;
+
+void setup() {
+}
+
+void loop() {
+  delay(800);
+  
+  for (i = 1000; i < 3500; i += 100)
+  {
+    tone(SPEAKER, i, 100);
+    delay(10);
+  }
+}
