de5d0f4c5e3cba6b4752587f4e088eb7b0673447
Sharon Cichelli <girlwritescode@gmail.com>
2015-11-03 08:02:21 -0600

    buzzer intensity responds to reading from force-sensing resistor

---
+++ SoundExamples/buzzer/buzzer.ino
@@ -0,0 +1,21 @@
+#define MOTOR 6
+#define FSR 0
+int forceReading;
+int motorSpeed;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(MOTOR, OUTPUT);
+  pinMode(FSR, INPUT);
+}
+
+void loop() {
+  forceReading = analogRead(FSR);
+  Serial.print("Analog reading: ");
+  Serial.println(forceReading);
+  
+  motorSpeed = map(forceReading, 800, 1023, 0, 255);
+  analogWrite(MOTOR, motorSpeed);
+  
+  delay(100);
+}
